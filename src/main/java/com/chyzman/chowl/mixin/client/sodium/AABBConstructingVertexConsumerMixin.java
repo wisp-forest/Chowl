113488d079061f7cfdc5ace438272c66aeb952ec
Basique <basiqueevangelist@yandex.ru>
2023-12-23 00:25:28 +0300

    we do a little trolling

---
+++ src/main/java/com/chyzman/chowl/mixin/client/sodium/AABBConstructingVertexConsumerMixin.java
@@ -0,0 +1,35 @@
+package com.chyzman.chowl.mixin.client.sodium;
+
+import com.chyzman.chowl.asm.OnlyWithMod;
+import com.chyzman.chowl.classes.AABBConstructingVertexConsumer;
+import net.caffeinemc.mods.sodium.api.vertex.attributes.CommonVertexAttribute;
+import net.caffeinemc.mods.sodium.api.vertex.attributes.common.PositionAttribute;
+import net.caffeinemc.mods.sodium.api.vertex.buffer.VertexBufferWriter;
+import net.caffeinemc.mods.sodium.api.vertex.format.VertexFormatDescription;
+import net.minecraft.client.render.VertexConsumer;
+import org.lwjgl.system.MemoryStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@OnlyWithMod(modid = "sodium")
+@Mixin(AABBConstructingVertexConsumer.class)
+public abstract class AABBConstructingVertexConsumerMixin implements VertexBufferWriter {
+    @Shadow
+    public abstract VertexConsumer vertex(double x, double y, double z);
+
+    @Override
+    public void push(MemoryStack stack, long ptr, int count, VertexFormatDescription format) {
+        long stride = format.stride();
+        long offsetPosition = format.getElementOffset(CommonVertexAttribute.POSITION);
+
+        for(int vertexIndex = 0; vertexIndex < count; ++vertexIndex) {
+            float x = PositionAttribute.getX(ptr + offsetPosition);
+            float y = PositionAttribute.getY(ptr + offsetPosition);
+            float z = PositionAttribute.getZ(ptr + offsetPosition);
+
+            vertex(x, y, z);
+
+            ptr += stride;
+        }
+    }
+}
