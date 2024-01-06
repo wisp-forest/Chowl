80c04892ff13a8be5ff8f93f57023ab2c088e14a
Basique <basiqueevangelist@yandex.ru>
2024-01-06 03:20:34 +0300

    ebic lavender hax

--- src/main/java/com/chyzman/chowl/mixin/lavender/LandingPageSupplierMixin.java
+++ src/main/java/com/chyzman/chowl/mixin/lavender/LandingPageSupplierMixin.java
@@ -1,26 +1,22 @@
 package com.chyzman.chowl.mixin.lavender;
 
 import com.chyzman.chowl.Chowl;
+import com.chyzman.chowl.pond.lavender.EntryAccess;
+import com.google.common.collect.ImmutableSet;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.wispforest.lavender.book.Category;
+import io.wispforest.lavender.book.Entry;
 import io.wispforest.lavender.book.LavenderBookItem;
 import io.wispforest.lavender.client.LavenderBookScreen;
-import io.wispforest.owo.ui.component.ItemComponent;
-import io.wispforest.owo.ui.component.LabelComponent;
-import io.wispforest.owo.ui.container.Containers;
-import io.wispforest.owo.ui.container.FlowLayout;
-import io.wispforest.owo.ui.core.*;
-import io.wispforest.owo.ui.util.UISounds;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Mixin(LavenderBookScreen.LandingPageSupplier.class)
 public abstract class LandingPageSupplierMixin extends LavenderBookScreen.PageSupplier {
@@ -33,85 +29,54 @@ public abstract class LandingPageSupplierMixin extends LavenderBookScreen.PageSu
         return ctx.book.id().getNamespace().equals(Chowl.MODID) || original;
     }
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lio/wispforest/lavender/client/LavenderBookScreen$LandingPageSupplier;buildEntryIndex(Ljava/util/Collection;[I)Ljava/util/List;"))
-    private void explosion(LavenderBookScreen context, CallbackInfo ci) {
-        if (!context.book.id().getNamespace().equals(Chowl.MODID)) return;
+    @ModifyExpressionValue(method = "<init>", at = @At(value = "INVOKE", target = "Lio/wispforest/lavender/book/Book;orphanedEntries()Ljava/util/Collection;"))
+    private Collection<Entry> malding(Collection<Entry> original, LavenderBookScreen context) {
+        if (!context.book.id().getNamespace().equals(Chowl.MODID)) return original;
 
-        var categoryContainer = Containers.verticalFlow(Sizing.fill(100), Sizing.content());
+        List<Entry> better = new ArrayList<>(original);
+
         var book = context.book;
         var client = MinecraftClient.getInstance();
 
-        book
-            .categories().stream()
-            .sorted(Comparator.comparingInt(Category::ordinal))
-            .sorted(Comparator.comparing($ -> !book.shouldDisplayCategory($, client.player)))
-            .forEach(category -> {
-//                if (book.shouldDisplayCategory(category, client.player)) {
-//                    categoryContainer.child(Components.item(category.icon()).<ItemComponent>configure(categoryButton -> {
-//                        categoryButton
-//                            .tooltip(Text.literal(category.title()))
-//                            .margins(Insets.of(4))
-//                            .cursorStyle(CursorStyle.HAND);
-//
-//                        categoryButton.mouseDown().subscribe((mouseX, mouseY, button) -> {
-//                            if (button != GLFW.GLFW_MOUSE_BUTTON_LEFT) return false;
-//
-//                            context.navPush(new LavenderBookScreen.CategoryPageSupplier(context, category));
-//                            UISounds.playInteractionSound();
-//                            return true;
-//                        });
-//                    }));
-//                } else if (!category.secret()) {
-//                    categoryContainer.child(this.context.template(Component.class, "locked-category-button"));
-//                }
+        for (Category category : book.categories()) {
+            Entry basedEntry = new Entry(
+                category.id(),
+                null,
+                category.title(),
+                category.icon(),
+                category.secret(),
+                category.ordinal(),
+                book.shouldDisplayCategory(category, client.player)
+                    ? ImmutableSet.of()
+                    : ImmutableSet.of(new Identifier("haha", "lmao")),
+                ImmutableSet.of(),
+                "explosion"
+            );
 
-                ParentComponent indexItem;
-                if (book.shouldDisplayCategory(category, client.player)) {
-                    indexItem = ((LavenderBookScreenAccessor) context).callTemplate(ParentComponent.class, "index-item");
-                    indexItem.childById(ItemComponent.class, "icon").stack(category.icon());
+            ((EntryAccess)(Object) basedEntry).chowl$setFake(null, () -> new LavenderBookScreen.CategoryPageSupplier(context, category));
 
-                    var label = indexItem.childById(LabelComponent.class, "index-label");
+            better.add(basedEntry);
+        }
 
-                    label.text(Text.literal(category.title()).styled($ -> $.withFont(MinecraftClient.UNICODE_FONT_ID)));
-                    label.mouseDown().subscribe((mouseX, mouseY, button) -> {
-                        if (button != GLFW.GLFW_MOUSE_BUTTON_LEFT) return false;
+        var allEntries = new Entry(
+            new Identifier("haha", "explosion"),
+            null,
+            "All Entries",
+            LavenderBookItem.itemOf(context.book),
+            false,
+            100,
+            ImmutableSet.of(),
+            ImmutableSet.of(),
+            "explosion"
+        );
 
-                        context.navPush(new LavenderBookScreen.CategoryPageSupplier(context, category));
-                        UISounds.playInteractionSound();
-                        return true;
-                    });
+        ((EntryAccess)(Object) allEntries).chowl$setFake(
+            Text.translatable("text.lavender.index_category").styled($ -> $.withFont(MinecraftClient.UNICODE_FONT_ID)),
+            () -> new LavenderBookScreen.IndexPageSupplier(context)
+        );
 
-                    var animation = label.color().animate(150, Easing.SINE, Color.ofFormatting(Formatting.GOLD));
-                    label.mouseEnter().subscribe(animation::forwards);
-                    label.mouseLeave().subscribe(animation::backwards);
-                } else {
-                    indexItem = ((LavenderBookScreenAccessor) context).callTemplate(ParentComponent.class, "locked-index-item");
-                    indexItem.childById(LabelComponent.class, "index-label").text(Text.translatable("text.lavender.entry.locked"));
-                }
+        better.add(allEntries);
 
-                categoryContainer.child(indexItem);
-            });
-
-        var allEntriesItem = ((LavenderBookScreenAccessor) context).callTemplate(ParentComponent.class, "index-item");
-        allEntriesItem.childById(ItemComponent.class, "icon").stack(LavenderBookItem.itemOf(book));
-
-        var label = allEntriesItem.childById(LabelComponent.class, "index-label");
-
-        label.text(Text.translatable("text.lavender.index_category").styled($ -> $.withFont(MinecraftClient.UNICODE_FONT_ID)));
-        label.mouseDown().subscribe((mouseX, mouseY, button) -> {
-            if (button != GLFW.GLFW_MOUSE_BUTTON_LEFT) return false;
-
-            context.navPush(new LavenderBookScreen.IndexPageSupplier(context));
-            UISounds.playInteractionSound();
-            return true;
-        });
-
-        var animation = label.color().animate(150, Easing.SINE, Color.ofFormatting(Formatting.GOLD));
-        label.mouseEnter().subscribe(animation::forwards);
-        label.mouseLeave().subscribe(animation::backwards);
-
-        categoryContainer.child(allEntriesItem);
-
-        ((FlowLayout) pages.get(1)).child(categoryContainer);
+        return better;
     }
 }
