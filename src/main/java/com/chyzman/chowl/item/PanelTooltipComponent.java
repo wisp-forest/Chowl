c15173de73402a64056fed82902385843f0f18ca
chyzman <32403637+chyzman@users.noreply.github.com>
2024-01-09 13:21:54 -0500

    remade panel tooltips

--- src/main/java/com/chyzman/chowl/item/PanelTooltipComponent.java
+++ src/main/java/com/chyzman/chowl/item/PanelTooltipComponent.java
@@ -3,18 +3,28 @@ package com.chyzman.chowl.item;
 import com.chyzman.chowl.item.component.CapacityLimitedPanelItem;
 import com.chyzman.chowl.item.component.DisplayingPanelItem;
 import com.chyzman.chowl.item.component.UpgradeablePanelItem;
+import com.chyzman.chowl.transfer.BigStorageView;
+import com.chyzman.chowl.transfer.FakeStorageView;
+import com.chyzman.chowl.transfer.PanelStorageContext;
 import com.chyzman.chowl.util.CompressionManager;
 import io.wispforest.owo.ui.base.BaseOwoTooltipComponent;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.core.Insets;
 import io.wispforest.owo.ui.core.Sizing;
 import io.wispforest.owo.ui.core.VerticalAlignment;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.chyzman.chowl.util.FormatUtil.formatCount;
 
 @SuppressWarnings("UnstableApiUsage")
 public class PanelTooltipComponent extends BaseOwoTooltipComponent<FlowLayout> {
@@ -22,47 +32,43 @@ public class PanelTooltipComponent extends BaseOwoTooltipComponent<FlowLayout> {
         super(() -> {
             var flow = Containers.verticalFlow(Sizing.content(), Sizing.content());
             if (stack.getItem() instanceof DisplayingPanelItem panel) {
-                var currentFilter = panel.displayedVariant(stack);
-                if (!currentFilter.isBlank()) {
-                    var currentFlow = Containers.horizontalFlow(Sizing.content(), Sizing.content());
-                    currentFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.filter.label")));
-                    if (panel instanceof CompressingPanelItem compressingPanel) {
-                        var node = CompressionManager.getOrCreateNode(compressingPanel.currentFilter(stack).getItem());
-                        while (node != null) {
-                            currentFlow.child(Components.item(node.item.getDefaultStack()).sizing(Sizing.fixed(MinecraftClient.getInstance().textRenderer.fontHeight)));
-                            node = node.next;
+                var storage = panel.getStorage(PanelStorageContext.forRendering(stack));
+                if (storage != null && !storage.getSlots().isEmpty()) {
+                    var filterFlow = Containers.verticalFlow(Sizing.content(), Sizing.content());
+                    filterFlow.margins(Insets.bottom(2));
+                    filterFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.contained.label")));
+                    List<StorageView<ItemVariant>> slots = new ArrayList<>(storage.getSlots());
+                    slots.removeIf(x -> x instanceof FakeStorageView);
+                    for (int i = 0; i < slots.size(); i++) {
+                        var slot = slots.get(i);
+                        var item = slot.getResource().getItem();
+                        var currentFilter = Containers.horizontalFlow(Sizing.content(), Sizing.content());
+                        currentFilter.child(Components.item(item.getDefaultStack())
+                                .margins(Insets.right(2))
+                                .sizing(Sizing.fixed(MinecraftClient.getInstance().textRenderer.fontHeight)));
+                        BigInteger count = BigStorageView.bigAmount(slot);
+                        StringBuilder countText = new StringBuilder();
+                        countText.append(formatCount(count));
+                        if (panel instanceof CapacityLimitedPanelItem cap && cap.capacity(stack).signum() > 0) {
+                            countText.append("/").append(formatCount(BigStorageView.bigCapacity(slot)));
                         }
-                    } else {
-                        currentFlow.child(Components.item(currentFilter.toStack()).sizing(Sizing.fixed(MinecraftClient.getInstance().textRenderer.fontHeight)));
+                        currentFilter.child(Components.label(Text.of(countText.toString())));
+                        filterFlow.child(currentFilter);
                     }
-                    currentFlow.verticalAlignment(VerticalAlignment.CENTER);
-                    flow.child(currentFlow);
-                }
-                var currentCount = panel.displayedCount(stack, null, null);
-                if (currentCount.compareTo(BigInteger.ZERO) > 0) {
-                    var currentFlow = Containers.horizontalFlow(Sizing.content(), Sizing.content());
-                    currentFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.count.label", currentCount.toString())));
-                    currentFlow.verticalAlignment(VerticalAlignment.CENTER);
-                    flow.child(currentFlow);
-                }
-            }
-            if (stack.getItem() instanceof CapacityLimitedPanelItem cap) {
-                var currentCapacity = cap.capacity(stack);
-                if (currentCapacity.compareTo(BigInteger.ZERO) > 0) {
-                    var currentFlow = Containers.horizontalFlow(Sizing.content(), Sizing.content());
-                    currentFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.capacity.label", currentCapacity.toString().substring(0, Math.min(1000, currentCapacity.toString().length())))));
-                    currentFlow.verticalAlignment(VerticalAlignment.CENTER);
-                    flow.child(currentFlow);
+                    flow.child(filterFlow);
                 }
             }
             if (stack.getItem() instanceof UpgradeablePanelItem panel) {
                 var upgrades = panel.upgrades(stack).stream().filter(stack1 -> !stack1.isEmpty()).toList();
                 if (!upgrades.isEmpty()) {
-                    var currentFlow = Containers.horizontalFlow(Sizing.content(), Sizing.content());
-                    currentFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.filter.upgrades")));
-                    upgrades.forEach(upgrade -> currentFlow.child(Components.item(upgrade).sizing(Sizing.fixed(MinecraftClient.getInstance().textRenderer.fontHeight))));
-                    currentFlow.verticalAlignment(VerticalAlignment.CENTER);
-                    flow.child(currentFlow);
+                    var upgradesFlow = Containers.verticalFlow(Sizing.content(), Sizing.content());
+                    upgradesFlow.margins(Insets.bottom(2));
+                    upgradesFlow.child(Components.label(Text.translatable("ui.chowl-industries.panel.tooltip.filter.upgrades.label")));
+                    var upgradesBox = Containers.horizontalFlow(Sizing.content(), Sizing.content());
+                    upgrades.forEach(upgrade -> upgradesBox.child(Components.item(upgrade).sizing(Sizing.fixed(MinecraftClient.getInstance().textRenderer.fontHeight))));
+                    upgradesBox.verticalAlignment(VerticalAlignment.CENTER);
+                    upgradesFlow.child(upgradesBox);
+                    flow.child(upgradesFlow);
                 }
             }
             return flow;
