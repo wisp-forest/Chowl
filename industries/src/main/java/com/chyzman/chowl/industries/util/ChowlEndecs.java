256925bff17add5dca3a381481dea244db398191
Basique <basiqueevangelist@yandex.ru>
2024-07-24 20:14:35 +0300

    I LOVE EDMOPS IT'S TOTALLY FREE OF BUGS

--- industries/src/main/java/com/chyzman/chowl/industries/util/ChowlEndecs.java
+++ industries/src/main/java/com/chyzman/chowl/industries/util/ChowlEndecs.java
@@ -1,6 +1,13 @@
 package com.chyzman.chowl.industries.util;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DynamicOps;
 import io.wispforest.endec.Endec;
+import io.wispforest.owo.serialization.RegistriesAttribute;
+import io.wispforest.owo.serialization.format.nbt.NbtEndec;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.registry.RegistryOps;
 
 import java.math.BigInteger;
 
@@ -11,6 +18,27 @@ public final class ChowlEndecs {
 
         return new BigInteger(str);
     }, BigInteger::toString);
+
+    public static <T> Endec<T> toEndecViaNbt(Codec<T> codec) {
+        return Endec.of(
+            (ctx, serializer, value) -> {
+                DynamicOps<NbtElement> ops = NbtOps.INSTANCE;
+                if (ctx.hasAttribute(RegistriesAttribute.REGISTRIES)) {
+                    ops = RegistryOps.of(ops, ctx.getAttributeValue(RegistriesAttribute.REGISTRIES).infoGetter());
+                }
+
+                NbtEndec.ELEMENT.encode(ctx, serializer, codec.encodeStart(ops, value).getOrThrow(IllegalStateException::new));
+            },
+            (ctx, deserializer) -> {
+                DynamicOps<NbtElement> ops = NbtOps.INSTANCE;
+                if (ctx.hasAttribute(RegistriesAttribute.REGISTRIES)) {
+                    ops = RegistryOps.of(ops, ctx.getAttributeValue(RegistriesAttribute.REGISTRIES).infoGetter());
+                }
+
+                return codec.parse(ops, NbtEndec.ELEMENT.decode(ctx, deserializer)).getOrThrow(IllegalStateException::new);
+            }
+        );
+    }
 
     private ChowlEndecs() {
 
