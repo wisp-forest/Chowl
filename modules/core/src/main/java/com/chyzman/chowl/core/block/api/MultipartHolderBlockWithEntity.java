54f7cfb69114caaea83f636544876e92cb420872
LunaFox <awakenedredstone@gmail.com>
2025-07-29 12:37:31 -0300

    fix: Add support for base block outline shape

--- modules/core/src/main/java/com/chyzman/chowl/core/block/api/MultipartHolderBlockWithEntity.java
+++ modules/core/src/main/java/com/chyzman/chowl/core/block/api/MultipartHolderBlockWithEntity.java
@@ -14,20 +14,26 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.util.shape.*;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public abstract class MultipartHolderBlockWithEntity extends BlockWithEntity {
     private final BlockEntityFactory<?> factory;
     private final @Nullable PartType<?> initialPart;
+    private static final VoxelShape UNSET = Util.make(() -> {
+        VoxelSet voxelSet = new BitSetVoxelSet(1, 1, 1);
+        voxelSet.set(0, 0, 0);
+        return new SimpleVoxelShape(voxelSet);
+    });
 
     protected MultipartHolderBlockWithEntity(BlockEntityFactory<?> factory, @Nullable PartType<?> initialPart, Settings settings) {
         super(settings);
@@ -55,13 +61,14 @@ public abstract class MultipartHolderBlockWithEntity extends BlockWithEntity {
     }
 
     protected VoxelShape getBlockOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.fullCube();
+        return UNSET;
     }
 
     @Override
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        VoxelShape blockOutlineShape = getBlockOutlineShape(state, world, pos, context);
         if (!(world.getBlockEntity(pos) instanceof MultipartHolderBlockEntity multipart)) {
-            return getBlockOutlineShape(state, world, pos, context);
+            return blockOutlineShape;
         }
 
         if (multipart.getShapeCache() != null) {
@@ -70,8 +77,10 @@ public abstract class MultipartHolderBlockWithEntity extends BlockWithEntity {
 
         // Copy the parts to a temp list so it doesn't ConcurrentModificationException
         ArrayList<Part> parts = new ArrayList<>(multipart.getParts());
-        List<VoxelShape> partsShapes = parts.stream().map(part -> part.getOutlineShape(parts, world, pos, context)).toList();
-        VoxelShape shape = new MultipartVoxelShape(partsShapes);
+        List<VoxelShape> partsShapes = parts.stream().map(part -> part.getOutlineShape(parts, world, pos, context)).collect(Collectors.toList());
+        boolean hasBaseShape = blockOutlineShape != UNSET;
+        if (hasBaseShape) partsShapes.addFirst(blockOutlineShape);
+        VoxelShape shape = new MultipartVoxelShape(partsShapes, hasBaseShape);
         multipart.setShapeCache(shape);
 
         return shape;
