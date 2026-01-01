446cb94aa1a2f0f2d91c6a91dcc54e23f23c94e3
chyzman <chyzalt@gmail.com>
2026-01-01 01:32:42 -0500

    all ready to update to 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -104,6 +104,7 @@ allprojects {
     dependencies {
         minecraft("com.mojang:minecraft:${rootProject.property("minecraft_version")}")
         mappings("net.fabricmc:yarn:${rootProject.property("yarn_mappings")}:v2")
+//        mappings(loom.officialMojangMappings())
 
         modImplementation("net.fabricmc:fabric-loader:${rootProject.property("loader_version")}")
         modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.property("fabric_version")}")
