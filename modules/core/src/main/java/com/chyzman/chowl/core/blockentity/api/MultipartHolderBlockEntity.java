ae3cff00d724915260e1beb7a8d6202a4e52c6f3
LunaFox <awakenedredstone@gmail.com>
2026-01-11 21:01:46 -0300

    chore: stop crash

--- modules/core/src/main/java/com/chyzman/chowl/core/blockentity/api/MultipartHolderBlockEntity.java
+++ modules/core/src/main/java/com/chyzman/chowl/core/blockentity/api/MultipartHolderBlockEntity.java
@@ -56,7 +56,7 @@ public abstract class MultipartHolderBlockEntity extends BlockEntity {
 
     public void addPart(Part part) {
         parts.add(part);
-        part.init(this);
+        if (!part.isInitialized()) part.init(this);
         markDirtyAndUpdateClients();
         clearShapeCache();
 
